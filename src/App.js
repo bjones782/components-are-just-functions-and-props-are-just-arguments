5ed578b5377349093633f100fe7011a6a6f5c02a
flatlandnick <nicholas.lunn@flatironschool.com>
2022-03-16 11:21:46 -0500

    readme

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import logo from "./logo.svg";
 // import "./App.css";
 import Header from "./Header.js";
 import Greeting from "./Greeting.js";
+import MultiPropComponent from "./MultiPropComponent";
 function App() {
   return (
     <div className="App">
       <Header />
       <Greeting name="Nick" />
       {Greeting({ name: "Archer" })}
+      <MultiPropComponent food="Ice Cream" color="Green" number={24} />
+      {MultiPropComponent({ food: "Wings", color: "Purple", number: 2 })}
     </div>
   );
 }
---
+++ src/MultiPropComponent.js
@@ -0,0 +1,9 @@
+function MultiPropComponent({ food, color, number }) {
+  return (
+    <p>
+      food: {food} color: {color} number: {number}
+    </p>
+  );
+}
+
+export default MultiPropComponent;
